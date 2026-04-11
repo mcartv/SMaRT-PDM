6d203c8790a3cbfb17feac44113e06d0a4abbfef
lolesciax <lolesciax@example.com>
2026-04-12 01:31:24 +0800

    Payout Schedule/Apply Now: Working

--- mobile/smartpdm_mobileapp/ios/Flutter/flutter_export_environment.sh
+++ mobile/smartpdm_mobileapp/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\ADMIN\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\projects\SMaRT-PDM\mobile\smartpdm_mobileapp"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Administrator\Desktop\Skulshits\IT32A\Capstone\SMaRT-PDM\mobile\smartpdm_mobileapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"

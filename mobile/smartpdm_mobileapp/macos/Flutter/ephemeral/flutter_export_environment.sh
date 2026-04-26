f6fdacd90dfc1aecb75bf5817f4cf35690cda8e4
mcartv <buenavidezcarlarthur@gmail.com>
2026-04-26 17:03:10 +0800

    main to dev-local merge (unfinished)

--- mobile/smartpdm_mobileapp/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/smartpdm_mobileapp/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\ADMIN\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\projects\SMaRT-PDM\mobile\smartpdm_mobileapp"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ADMIN\Downloads\SMaRT-PDM\mobile\smartpdm_mobileapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"

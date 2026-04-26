e0d391deed4d41a49d5e14ce6dae3b8ca445e206
vncvppp <vncvppp@gmail.com>
2026-04-26 22:06:11 +0800

    feat: implement AppConfig for API base URL management and reCAPTCHA site key configuration

--- mobile/smartpdm_mobileapp/lib/core/config/app_config.dart
+++ mobile/smartpdm_mobileapp/lib/core/config/app_config.dart
@@ -3,8 +3,9 @@ import 'package:flutter/foundation.dart';
 class AppConfig {
   AppConfig._();
 
-  static const String _defaultDebugBaseUrl = 'http://192.168.22.2:5000';
+  //static const String _defaultDebugBaseUrl = 'http://192.168.22.2:5000';
   static const String _defaultReleaseBaseUrl = 'https://smart-pdm-mipx.onrender.com';
+  static const String _defaultDebugBaseUrl = 'https://smart-pdm-mipx.onrender.com';
 
   static String get apiBaseUrl {
     const configuredValue = String.fromEnvironment('API_BASE_URL');
